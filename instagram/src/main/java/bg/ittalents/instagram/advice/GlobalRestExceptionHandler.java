9582b057f5045046151bca52f35bc9df089f3ecb
SashoTsolov <81829474+vulgar-chair@users.noreply.github.com>
2023-04-13 15:29:28 +0300

    add modelmapper dependency to pom.xml and GlobalRestExceptionHandler class

---
+++ instagram/src/main/java/bg/ittalents/instagram/advice/GlobalRestExceptionHandler.java
@@ -0,0 +1,10 @@
+package bg.ittalents.instagram.advice;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalRestExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+}
