7c8ebdc0a8d9a8e3e3a0c7317a096d1f977c9c67
Iliya <lamzesmainacc@gmail.com>
2023-04-13 20:53:40 +0300

    add several userDTOs, UserEntity, UserController, UserRepo, UserService and AbstractController

---
+++ instagram/src/main/java/bg/ittalents/instagram/user/User.java
@@ -0,0 +1,57 @@
+package bg.ittalents.instagram.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String username;
+    @Column
+    private String password;
+    @Column
+    private String email;
+    @Column
+    private String name;
+    @Column
+    private String bio;
+    @Column
+    private Date dateOfBirth;
+    @Column
+    private char gender;
+    @Column
+    private String profilePictureUrl;
+    @Column
+    private boolean isVerified;
+    @Column
+    private String verificationCode;
+    @Column
+    private Timestamp dateTimeCreated;
+    @Column
+    private boolean isDeactivated;
+    @OneToMany(mappedBy = "owner")
+    private List<Post> posts;
+
+    @ManyToMany
+    @JoinTable(
+        name = "users_block_users",
+        joinColumns = @JoinColumn(name = "blocked_user_id"),
+        inverseJoinColumns = @JoinColumn(name = "blocking_user_id")
+    )
+    private Set<User> blocked = new HashSet<>();
+
+    private Set<User> followers = new HashSet<>();
+    //TODO
+}
---
+++ instagram/src/main/java/bg/ittalents/instagram/util/AbstractController.java
@@ -0,0 +1,14 @@
+package bg.ittalents.instagram.util;
+
+import bg.ittalents.instagram.exceptions.UnauthorizedException;
+import jakarta.servlet.http.HttpSession;
+
+public abstract class AbstractController {
+
+    protected long getLoggedId(HttpSession s) {
+        if (s.getAttribute("LOGGED_ID") == null) {
+            throw new UnauthorizedException("You have to login first");
+        }
+        return (long) s.getAttribute("LOGGED_ID");
+    }
+}
