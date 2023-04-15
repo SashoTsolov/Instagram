547fc7e88c6833c48779c34af54690a0b9111175
Iliya <lamzesmainacc@gmail.com>
2023-04-16 02:30:18 +0300

    add methods in UserController and UserService and do small updates on other classes

--- instagram/src/main/java/bg/ittalents/instagram/user/DTOs/UserBasicInfoDTO.java
+++ instagram/src/main/java/bg/ittalents/instagram/user/DTOs/UserBasicInfoDTO.java
@@ -3,6 +3,6 @@ package bg.ittalents.instagram.user.DTOs;
 public record UserBasicInfoDTO(
         Long id,
         String username,
-        int name,
-        String profilePictureUrl) {}
-
+        String name,
+        String profilePictureUrl
+) {}
