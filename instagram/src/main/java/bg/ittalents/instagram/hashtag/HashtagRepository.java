e3914431bbae9744c7394c5752c4e72b16dcb83d
SashoTsolov <81829474+vulgar-chair@users.noreply.github.com>
2023-04-16 21:19:16 +0300

    add Media, Hashtag, Location repositories

---
+++ instagram/src/main/java/bg/ittalents/instagram/hashtag/HashtagRepository.java
@@ -0,0 +1,11 @@
+package bg.ittalents.instagram.hashtag;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
+
+    Optional<Hashtag> findByName(String name);
+    boolean existsByName(String name);
+}
--- instagram/src/main/java/bg/ittalents/instagram/location/Location.java
+++ instagram/src/main/java/bg/ittalents/instagram/location/Location.java
@@ -1,30 +1,33 @@
 package bg.ittalents.instagram.post.entities;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Getter
 @Setter
-@Entity(name = "media")
-public class Media {
-
+@Entity(name = "locations")
+public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "media_url", nullable = false)
-    private String mediaUrl;
+    @Column(nullable = false)
+    private String name;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "post_id", nullable = false)
-    private Post post;
+    @OneToMany(mappedBy = "location")
+    private Set<Post> posts;
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
+
