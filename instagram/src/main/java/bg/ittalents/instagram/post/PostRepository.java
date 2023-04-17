3d0914dfd2dcbf657c6f2b59ef750b185f0dfd1a
SashoTsolov <81829474+vulgar-chair@users.noreply.github.com>
2023-04-17 16:28:53 +0300

    change Post and Hashtag repositories

--- instagram/src/main/java/bg/ittalents/instagram/post/PostRepository.java
+++ instagram/src/main/java/bg/ittalents/instagram/post/PostRepository.java
@@ -1,7 +1,10 @@
 package bg.ittalents.instagram.post;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +20,33 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             ORDER BY date_time_created DESC
             """, nativeQuery = true)
     List<Post> findByOwnerIdOrderByUploadDateDesc(Long ownerId);
+
+    @Query(value = """
+            SELECT 
+                p.* 
+            FROM posts p 
+            JOIN posts_have_hashtags phh 
+            ON p.id = phh.post_id 
+            JOIN hashtags h 
+            ON phh.hashtag_id = h.id 
+            WHERE h.name = :hashtagName AND p.is_created = 1
+            ORDER BY p.date_time_created DESC""",
+            nativeQuery = true)
+    Page<Post> findByHashtagNameSortedByDateTimeCreatedDesc(@Param("hashtagName") String hashtagName,
+                                                            Pageable pageable);
+
+
+    @Query(value = """
+            SELECT 
+                p.* 
+            FROM posts p 
+            JOIN locations l 
+            ON p.location_id = l.id 
+            WHERE l.name = :locationName AND p.is_created = 1
+            ORDER BY p.date_time_created 
+            DESC""",
+            nativeQuery = true)
+    Page<Post> findByLocationNameSortedByDateTimeCreatedDesc(@Param("locationName") String locationName,
+                                                             Pageable pageable);
+
 }
