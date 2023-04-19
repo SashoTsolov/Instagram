e13909c3ec39ba167824eb2a661fbbf1ef63c7af
Iliya <lamzesmainacc@gmail.com>
2023-04-19 19:39:19 +0300

    add final changes v1

--- instagram/src/main/java/bg/ittalents/instagram/util/AbstractService.java
+++ instagram/src/main/java/bg/ittalents/instagram/util/AbstractService.java
@@ -1,11 +1,13 @@
 package bg.ittalents.instagram.util;
 
 import bg.ittalents.instagram.exceptions.NotFoundException;
+import bg.ittalents.instagram.follower.FollowRepository;
 import bg.ittalents.instagram.post.PostRepository;
-import bg.ittalents.instagram.post.PostService;
 import bg.ittalents.instagram.user.UserRepository;
+import jakarta.persistence.EntityManager;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import bg.ittalents.instagram.user.User;
 
@@ -21,9 +23,17 @@ public abstract class AbstractService {
     @Autowired
     protected ModelMapper mapper;
 
+    @Autowired
+    protected FollowRepository followRepository;
 
+    @Autowired
+    protected JavaMailSender javaMailSender;
 
     protected User getUserById(long id){
         return userRepository.findById(id).orElseThrow(() -> new NotFoundException("User was not found"));
+    }
+
+    protected User getUserByEmail(String email){
+        return userRepository.findByEmail(email).orElseThrow(() -> new NotFoundException("User was not found"));
     }
 }
